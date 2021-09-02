04195cc3c29759bd85cd9013bffbc5a3db2c85a0
Mayer <mayer.rafa@outlook.com>
2021-09-01 23:05:25 -0300

    atualizada lógica para admin

--- javascript/admin.js
+++ javascript/admin.js
@@ -3,17 +3,53 @@ let cars = [
         "placa": "FQJ-12525",
         "dataEntrada": "08-31-2021 16h",
         "dataSaida": "08-31-2021 17h",
-        "ticketStatus": "Pago"
+        "ticket": "0522ASD4174",
+        "ticketStatus": "Pago",
+        "valorPago": 10.29,
     },
     {
         "placa": "FQJ-12525",
         "dataEntrada": "08-31-2021 16h",
         "dataSaida": "08-31-2021 17h",
-        "ticketStatus": "Pago"
+        "ticket": "0522ASD4174",
+        "ticketStatus": "Pago",
+        "valorPago": 10.29,
+    },
+    {
+        "placa": "FQJ-12525",
+        "dataEntrada": "08-31-2021 16h",
+        "dataSaida": "08-31-2021 17h",
+        "ticket": "0522ASD4174",
+        "ticketStatus": "Não pago",
+        "valorPago": 10.29,
+    },
+    {
+        "placa": "FQJ-12525",
+        "dataEntrada": "08-31-2021 16h",
+        "dataSaida": "08-31-2021 17h",
+        "ticket": "0522ASD4174",
+        "ticketStatus": "Não pago",
+        "valorPago": 10.29,
+    },
+    {
+        "placa": "FQJ-12525",
+        "dataEntrada": "08-31-2021 16h",
+        "dataSaida": "08-31-2021 17h",
+        "ticket": "0522ASD4174",
+        "ticketStatus": "Pago",
+        "valorPago": 28.45,
     },
 ]
 
-let tableResumo = document.getElementById('tabela-corpo')
+let tableResumoEl = document.getElementById('tabela-corpo')
+let lucroEl = document.getElementById('lucroTotal')
+let qtdPagosEl = document.getElementById('qtdPagos')
+let qtdNaoPagosEl = document.getElementById('qtdNaoPagos')
+let qtdTotalEl = document.getElementById('qtdTotal')
+
+let lucroTotal = 0
+let qtdPagos = 0
+let qtdNaoPagos = 0
 
 cars.forEach(obj => {
     let template = `
@@ -21,8 +57,20 @@ cars.forEach(obj => {
             <td>${obj["placa"]}</td>
             <td>${obj["dataEntrada"]}</td>
             <td>${obj["dataSaida"]}</td>
+            <td>${obj["ticket"]}</td>
             <td>${obj["ticketStatus"]}</td>
+            <td>R$ ${obj["valorPago"].toFixed(2)}</td>
         </tr>
     `
-    tableResumo.innerHTML += template
-});
+    tableResumoEl.innerHTML += template
+    lucroTotal += obj["valorPago"]
+
+    if(obj["ticketStatus"] === "Pago"){
+        qtdPagos += 1
+    }
+})
+
+lucroEl.innerHTML = "R$ " + lucroTotal.toFixed(2)
+qtdPagosEl.innerHTML = qtdPagos
+qtdNaoPagosEl.innerHTML = cars.length - qtdPagos
+qtdTotalEl.innerHTML = cars.length

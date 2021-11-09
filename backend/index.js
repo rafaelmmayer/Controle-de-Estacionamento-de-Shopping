85074e46a280b3a086472c4b801d34094f62635c
Rafael <mayer.rafa@outlook.com>
2021-11-09 11:55:19 -0300

    Adionado filtros no metodo get

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ app.use(express.json())
 oracledb.autoCommit = true;
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
+const defaultRoute = '/api/tickets'
+
 async function connect() {    
     return await oracledb.getConnection({
         user: dbConfig.dbuser,
@@ -18,16 +20,53 @@ async function connect() {
     });
 }
 
-app.get('/', async (req, res) => {
+// Retorna todos os ticket
+app.get(defaultRoute, async (req, res) => {
+    let connection
     try{
-        let connection = await connect()
-        const result = await connection.execute('select * from ticket where id=1')
+        let sql = 'select * from ticket'
+        let result
+        const queryParams = req.query
+        connection = await connect()
+        if(queryParams.status){
+            result = await connection.execute(`${sql} where STATUS=:status`, queryParams)
+        }
+        else if(queryParams.finalizados) {
+            result = await connection.execute(`${sql} where DATA_SAIDA is not null`)
+        }
+        else{
+            result = await connection.execute(sql)
+        }
         res.send(result.rows)
     } catch (err){
         console.log(err)
-        res.status(500).send('Internal Error')
+        res.status(500).send('Erro interno')
+    } finally {
+        connection.close()        
     }
 
+})
+
+// Adiciona ticket no banco de dados
+app.post(defaultRoute, async (req, res) => {
+    let connection
+    try {
+        const ticket = req.body
+        ticket.DATA_ENTRADA = new Date()
+        connection = await connect()
+        const result = await connection.execute(
+            `insert into tickets 
+            (CODIGO, STATUS, DATA_ENTRADA, PLACA) values 
+            (:CODIGO, :STATUS, :DATA_ENTRADA, :PLACA)`,
+            ticket
+        )
+        res.status(201).send("Ticket criado")
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('Erro interno')
+    } finally {
+        connection.close()
+    }
 })
 
 app.listen(80, function () {

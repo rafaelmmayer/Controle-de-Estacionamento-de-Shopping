eb0048a0c10d6ec39f08a3a2b8770e1ba107c63c
Rafael <mayer.rafa@outlook.com>
2021-11-10 21:46:55 -0300

    finalizando backend para teste

--- backend/index.js
+++ backend/index.js
@@ -20,28 +20,50 @@ async function connect() {
     });
 }
 
+function generateTicket() {
+    return 
+}
+
 app.get(defaultRoute, async (req, res) => {
     let connection
     try{
         let result
         const queryParams = req.query
         connection = await connect()
-        if(queryParams.status){
+        if(queryParams.status){ // retorna tickets com status que veio da query
             result = await connection.execute(
-                'select PLACA, DATA_ENTRADA, VALOR_TOTAL from tickets where STATUS=:status',
+                'select PLACA, DATA_ENTRADA, VALOR_TOTAL, CODIGO from tickets where STATUS=:status',
                 { status: queryParams.status }
             )
-        }
-        else if(queryParams.codigo){
-            result = await connection.execute(
-                'select * from tickets where CODIGO=:codigo and ROWNUM=1',
-                { codigo: queryParams.codigo }
-            )         
         }
         else{ // retornar todos os tickets
             result = await connection.execute('select * from tickets')
         }
         res.send(result.rows)
+    } catch (err){
+        console.log(err)
+        res.status(500).send('Erro interno')
+    } finally {
+        connection.close()        
+    }
+})
+
+// retorna ticket a partir de um código
+app.get(`${defaultRoute}/:codigo`, async (req, res) => {
+    let connection
+    try{
+        const codigo = req.params.codigo
+        connection = await connect()
+        let result = await connection.execute(
+            'select * from tickets where CODIGO=:codigo and ROWNUM=1',
+            { codigo: codigo }
+        )
+        if(result.rows.length > 0){
+            res.send(result.rows[0])
+        }
+        else{
+            res.status(404).send(`Ticket ${codigo} não encontrado`)
+        }
     } catch (err){
         console.log(err)
         res.status(500).send('Erro interno')
@@ -56,7 +78,7 @@ app.get(`${defaultRoute}/finalizados`, async (req, res) => {
     try{
         connection = await connect()
         let result = await connection.execute(
-            'select PLACA, DATA_ENTRADA, DATA_SAIDA, VALOR_TOTAL from tickets where DATA_SAIDA is not null'
+            'select PLACA, DATA_ENTRADA, DATA_SAIDA, VALOR_TOTAL, CODIGO from tickets where DATA_SAIDA is not null'
         )
         res.send(result.rows)
     } catch (err){
@@ -71,16 +93,19 @@ app.get(`${defaultRoute}/finalizados`, async (req, res) => {
 app.post(defaultRoute, async (req, res) => {
     let connection
     try {
-        const ticket = req.body
-        ticket.DATA_ENTRADA = new Date()
+        let ticket = {
+            codigo: generateTicket(),
+            dataEntrada: new Date(),
+            placa: req.body.placa
+        }
         connection = await connect()
         await connection.execute(
             `insert into tickets 
             (CODIGO, STATUS, DATA_ENTRADA, PLACA) values 
-            (:CODIGO, 0, :DATA_ENTRADA, :PLACA)`,
+            (:codigo, 0, :dataEntrada, :placa)`,
             ticket
         )
-        res.status(201).send("Ticket criado")
+        res.status(201).send(`Ticket ${ticket.codigo} criado`)
     } catch (err) {
         console.log(err)
         res.status(500).send('Erro interno')
@@ -89,7 +114,7 @@ app.post(defaultRoute, async (req, res) => {
     }
 })
 
-// Atualiza ticket depois do pagamento
+// realiza pagamento de um ticket
 app.put(`${defaultRoute}/pagamento/`, async (req, res) => {
     let connection
     try {
@@ -99,14 +124,12 @@ app.put(`${defaultRoute}/pagamento/`, async (req, res) => {
             'select ID from tickets where CODIGO=:codigo and ROWNUM=1',
             { codigo: body.codigo }
         )
-
         if(result.rows.length > 0) {
             let id = result.rows[0].ID
             await connection.execute(
-                'update tickets set VALOR_TOTAL=:valorTotal where ID=:id',
+                'update tickets set VALOR_TOTAL=:valorTotal, STATUS=1 where ID=:id',
                 { valorTotal: body.valorTotal, id: id }
             )
-
             res.status(200).send(`Ticket ${body.codigo} pago`)
         }else {
             res.status(404).send(`Ticket ${body.codigo} não encontrado`)
@@ -126,16 +149,20 @@ app.put(`${defaultRoute}/saida/:codigo`, async (req, res) => {
         const codigo = req.params.codigo
         connection = await connect()
         let result = await connection.execute(
-            'select ID from tickets where CODIGO=:codigo and ROWNUM=1',
+            'select ID, STATUS from tickets where CODIGO=:codigo and ROWNUM=1',
             { codigo: codigo }
         )
-
         if(result.rows.length > 0) {
-            await connection.execute(
-                'update tickets set DATA_SAIDA=:dataSaida where ID=:id',
-                { dataSaida: new Date(), id: result.rows[0].ID }
-            )
-            res.status(200).send(`Ticket ${codigo} liberado`)
+            let ticket = result.rows[0]
+            if(ticket.STATUS == 1){
+                await connection.execute(
+                    'update tickets set DATA_SAIDA=:dataSaida where ID=:id',
+                    { dataSaida: new Date(), id: ticket.ID }
+                )
+                res.status(200).send(`Ticket ${codigo} liberado`)
+            } else {
+                res.status(400).send(`Ticket ${codigo} não pago`)
+            }            
         }else {
             res.status(404).send(`Ticket ${codigo} não encontrado`)
         }

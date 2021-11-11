37ced34944858f03301ff4ad62342512587f2932
Rafael <mayer.rafa@outlook.com>
2021-11-10 21:47:13 -0300

    finalizado paginas de admin

---
+++ frontend/javascript/admin não pagos.js
@@ -0,0 +1,28 @@
+let tableResumoEl = document.getElementById('tabela-corpo')
+
+function formatDateStr (strDate) {
+    if(!strDate) {
+        return ""
+    }
+    let date = new Date(strDate)
+    return `${date.getDate().toString().padStart(2, "0")}/${(date.getMonth()+1).toString().padStart(2, "0")}/${date.getFullYear()} 
+            ${date.getHours().toString().padStart(2, "0")}h${date.getMinutes().toString().padStart(2, "0")}`
+}
+
+async function fetchTicket() {
+    let response = await fetch('http://localhost:80/api/tickets?status=0');
+    let tickets = await response.json();
+
+    tickets.forEach(obj => {
+        let template = `
+            <tr>
+                <td>${obj.PLACA}</td>
+                <td>${formatDateStr(obj.DATA_ENTRADA)}</td>
+                <td>${obj.CODIGO}</td>
+            </tr>
+        `
+        tableResumoEl.innerHTML += template
+    })
+}
+
+fetchTicket()
---
+++ frontend/javascript/admin pagos.js
@@ -0,0 +1,29 @@
+let tableResumoEl = document.getElementById('tabela-corpo')
+
+function formatDateStr (strDate) {
+    if(!strDate) {
+        return ""
+    }
+    let date = new Date(strDate)
+    return `${date.getDate().toString().padStart(2, "0")}/${(date.getMonth()+1).toString().padStart(2, "0")}/${date.getFullYear()} 
+            ${date.getHours().toString().padStart(2, "0")}h${date.getMinutes().toString().padStart(2, "0")}`
+}
+
+async function fetchTicket() {
+    let response = await fetch('http://localhost:80/api/tickets?status=1');
+    let tickets = await response.json();
+
+    tickets.forEach(obj => {
+        let template = `
+            <tr>
+                <td>${obj.PLACA}</td>
+                <td>${formatDateStr(obj.DATA_ENTRADA)}</td>
+                <td>${obj.CODIGO}</td>
+                <td>R$ ${obj.VALOR_TOTAL.toFixed(2)}</td>
+            </tr>
+        `
+        tableResumoEl.innerHTML += template
+    })
+}
+
+fetchTicket()

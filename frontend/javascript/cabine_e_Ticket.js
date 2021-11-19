7e719c8fcaf2dc3984db2c73ee6389a8a011755f
Rafael <mayer.rafa@outlook.com>
2021-11-18 22:37:31 -0300

    teste cabine de entrada

--- frontend/javascript/cabine_e_Ticket.js
+++ frontend/javascript/cabine_e_Ticket.js
@@ -1,23 +1,61 @@
-var teste = [];
+const url = 'http://localhost:80/api/tickets'
+
+function formatDateStr (strDate) {
+    if(!strDate) {
+        return ""
+    }
+    let date = new Date(strDate)
+    return `${date.getDate().toString().padStart(2, "0")}/${(date.getMonth()+1).toString().padStart(2, "0")}/${date.getFullYear()} 
+            ${date.getHours().toString().padStart(2, "0")}h${date.getMinutes().toString().padStart(2, "0")}`
+}
+
 function bntvalid() {
     
     var input = document.querySelector("#nome");
 
-    console.log(input);
-  
     var iMercoPlaca = input.value;
 
     var iMercoMoto = input.value;
 
     if (validaPlacaCarro(iMercoPlaca)){
-        window.location.href = "./ticket-Entrada.html"
+        salvaPlaca(iMercoPlaca);
+        //window.location.href = "./ticket-Entrada.html"
     } else {
         if (validaPlacaMoto(iMercoMoto)){
-        window.location.href = "./ticket-Entrada.html"
+            salvaPlaca(iMercoMoto);
+        //window.location.href = "./ticket-Entrada.html"
         } else {
-            console.log(alert('Número digitado não condiz com placa merco sul'));
+            alert('Número digitado não condiz com placa merco sul');
         };
     };
+}
+
+function salvaPlaca(placa) {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            placa: placa
+        })
+    })
+    .then(res => {
+        if(res.status == 201) {
+            return res.json()
+        }        
+        else {
+            throw Error('Erro interno, tente novamente!');
+        }
+    })
+    .then(data => {
+        document.querySelector(".span-ticket").innerHTML = data;
+        document.querySelector(".span-data").innerHTML = formatDateStr(new Date());
+    })
+    .catch((err) => {
+        alert(err);
+    });
 }
 
 function validaPlacaCarro(placaCarro){
